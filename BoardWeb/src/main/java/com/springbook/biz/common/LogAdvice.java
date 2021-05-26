92b3cb5dfc844372af63edc557541cc042a9b476
dev-HardenLee <Harden.lee@companywe.co.kr>
2021-05-26 17:41:24 +0900

    AOP JoinPoint, Binding 변수

--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,11 +1,28 @@
 package com.springbook.biz.common;
 
 import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
+@Aspect
 public class LogAdvice {
 	
-	public void printLog(Joinpoint jp) {
+	/**
+	 * XML로 할 떄
+	 */
+//	public void printLog(Joinpoint jp) {
+//		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+//	}
+	
+	@Before("allPointCut()")
+	public void printLog() {
 		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
 	}
+	
+	@Pointcut("execution(* com.springbook.biz..*impl.*(..))")
+	public void allPointCut() {}
 	
 }
