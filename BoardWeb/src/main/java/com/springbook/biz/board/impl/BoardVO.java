b5deed5aca93ca37d6983b6c99b0833408066308
dev-HardenLee <Harden.lee@companywe.co.kr>
2021-06-07 20:19:00 +0900

    JSON, MyBatis 스터디

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardVO.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardVO.java
@@ -2,17 +2,25 @@ package com.springbook.biz.board.impl;
 
 import java.util.Date;
 
+
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//@XmlAccessorType(XmlAccessType.FIELD)
 public class BoardVO {
+	//@XmlAttribute
 	private int seq;
 	private String title;
 	private String writer;
 	private String content;
 	private Date regDate;
 	private int cnt;
+	//@XmlTransient
 	private String searchCondition;
+	//@XmlTransient
 	private String searchKeyword;
+	//@XmlTransient
 	private MultipartFile uploadFile;
 	
 	public int getSeq() {
@@ -33,12 +41,15 @@ public class BoardVO {
 	public int getCnt() {
 		return cnt;
 	}
+	@JsonIgnore
 	public String getSearchCondition() {
 		return searchCondition;
 	}
+	@JsonIgnore
 	public String getSearchKeyword() {
 		return searchKeyword;
 	}
+	@JsonIgnore
 	public MultipartFile getUploadFile() {
 		return uploadFile;
 	}
--- BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -3,6 +3,7 @@ package com.springbook.view.board;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.springbook.biz.board.impl.BoardListVO;
 import com.springbook.biz.board.impl.BoardService;
 import com.springbook.biz.board.impl.BoardVO;
 
@@ -82,7 +85,30 @@ public class BoardController {
 		model.addAttribute("boardList", boardService.getBoardList(vo));
 		return "getBoardList.jsp";
 	}
+	
+	@RequestMapping("/dataTransform.do")
+	@ResponseBody
+	public List<BoardVO> dataTransform(BoardVO vo){
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardService.getBoardList(vo);
+		return boardList;
+	}// dataTransform
+	
+//	@RequestMapping("/dataTransform.do")
+//	@ResponseBody
+//	public BoardListVO dataTransform(BoardVO vo){
+//		vo.setSearchCondition("TITLE");
+//		vo.setSearchKeyword("");
+//		List<BoardVO> boardList = boardService.getBoardList(vo);
+//		BoardListVO boardListVO = new BoardListVO();
+//		boardListVO.setBoardList(boardList);
+//		return boardListVO;
+//	}// dataTransform
+	
 }
+
+
 
 
 
